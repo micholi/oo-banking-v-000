84bc3ac4fb178504b559313849ea9bd0359f0b5f
Michelle Olivieri <michelleo72@gmail.com>
2018-03-29 23:53:04 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -21,13 +21,10 @@ attr_accessor :sender, :receiver, :status, :amount
     if sender.valid? == false
     self.status = "rejected"
 
-
-
 end
 
   def execute_transaction
-  if sender.valid? == false
-    bad_transfer
+  if bad_transfer
   "Transaction rejected. Please check your account balance."
 elsif valid? && self.status == "pending"
       sender.balance -= amount
