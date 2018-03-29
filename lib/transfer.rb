07041377c1cc04d924878b2c08018f2294042ca9
Michelle Olivieri <michelleo72@gmail.com>
2018-03-29 23:42:50 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,10 +18,10 @@ attr_accessor :sender, :receiver, :status, :amount
   def bad_transfer
     #BankAccount.all.find {|account| account.name != sender}.valid?
     #sender.valid? == false
-    
+
     self.status = "rejected"
     "Transaction rejected. Please check your account balance."
-    binding.pry
+    
 
 end
 
