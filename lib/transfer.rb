4f6703aeaab7a561c4c433ac76b08a0952e4006a
Michelle Olivieri <michelleo72@gmail.com>
2018-03-29 23:37:53 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,39 @@
+require 'pry'
+
 class Transfer
-  # your code here
+
+attr_accessor :sender, :receiver, :status, :amount
+
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @status = "pending"
+    @amount = amount
+  end
+
+  def valid?
+    BankAccount.all.find {|account| account.name == sender}.valid?
+    sender.valid? && receiver.valid?
+  end
+
+  def bad_transfer
+    #BankAccount.all.find {|account| account.name != sender}.valid?
+    #sender.valid? == false
+    self.status = "rejected"
+    "Transaction rejected. Please check your account balance."
+  end
+
+  def execute_transaction
+  #  if bad_transfer
+  #    "Transaction rejected. Please check your account balance."
+    if valid? && self.status == "pending"
+      sender.balance -= amount
+      receiver.balance += amount
+      self.status = "complete"
+
+    elsif bad_transfer
+"Transaction rejected. Please check your account balance."
+    end
+end
+
 end
