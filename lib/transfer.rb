1827cb9a21083b0b12515d0a408ff64f6b3af599
Michelle Olivieri <michelleo72@gmail.com>
2018-03-30 00:19:49 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -32,6 +32,7 @@ attr_accessor :sender, :receiver, :status, :amount
     if self.status == "complete"
       sender.balance += amount
       receiver.balance -= amount
+      self.status == "reversed"
     end
   end
 
